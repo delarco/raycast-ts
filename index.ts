f3ad9d82dc79b4c14a3b73e3897d03c0754e0f24
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-18 00:10:46 -0300

    update index exports

---
+++ index.ts
@@ -0,0 +1,5 @@
+export type { Position } from "./interfaces/Position"
+export type { Size } from "./interfaces/Size"
+export { Color } from "./models/Color"
+export { GameConfig } from "./GameConfig"
+export { Game } from "./Game"
