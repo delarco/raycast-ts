d530947c1791910cd66a2771876e7c23a192d807
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-19 00:03:45 -0300

    add size property and getTile method in Map class

--- src/models/Map.ts
+++ src/models/Map.ts
@@ -1,4 +1,15 @@
+import { Size } from "../interfaces/Size"
+
 export class Map {
 
     public tiles: Array<number>
+    public size: Size
+
+    public getTile(x: number, y: number): number | null {
+
+        if (x >= this.size.width) return null
+        if (y >= this.size.height) return null
+
+        return this.tiles[x + (y * this.size.width)]
+    }
 }
