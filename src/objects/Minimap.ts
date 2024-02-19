b3aa313134fd067941815a3eee76f26bad389ea9
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-19 05:43:37 -0300

    refactor Minimap to draw camera indicator

--- src/objects/Minimap.ts
+++ src/objects/Minimap.ts
@@ -1,4 +1,6 @@
+import { Position } from "../interfaces/Position";
 import { Renderer } from "../interfaces/Renderer";
+import { Camera } from "../models/Camera";
 import { Color } from "../models/Color";
 import { GameObject } from "../models/GameObject";
 import { Map } from "../models/Map";
@@ -8,7 +10,7 @@ export class Minimap extends GameObject {
     private tileSize: number
     private tileColor = new Color(100, 100, 100)
 
-    constructor(x: number, y: number, width: number, height: number, private map: Map) {
+    constructor(x: number, y: number, width: number, height: number, private map: Map, private camera: Camera) {
         super()
 
         this.x = x
@@ -20,16 +22,22 @@ export class Minimap extends GameObject {
 
     public override draw(renderer: Renderer): void {
 
+        this.drawTiles(renderer)
+        this.drawCamera(renderer)
+    }
+
+    private drawTiles(renderer: Renderer): void {
+
         for (const row of new Array(this.map.size.height).keys()) {
             for (const col of new Array(this.map.size.width).keys()) {
 
                 const tile = this.map.getTile(col, row)
 
-                if(tile === 1) {
+                if (tile === 1) {
 
                     renderer.drawRect(
-                        this.x + col *  this.tileSize,
-                        this.y + row *  this.tileSize,
+                        this.x + col * this.tileSize,
+                        this.y + row * this.tileSize,
                         this.tileSize,
                         this.tileSize,
                         this.tileColor
@@ -37,5 +45,30 @@ export class Minimap extends GameObject {
                 }
             }
         }
+    }
+
+    private drawCamera(renderer: Renderer): void {
+
+        const ratio: Position = {
+            x: this.tileSize / this.scene.gameInstance.UNIT_SIZE,
+            y: this.tileSize / this.scene.gameInstance.UNIT_SIZE,
+            z: 0
+        }
+
+        const cameraSize = this.tileSize / 3
+
+        const cameraPos: Position = {
+            x: this.x + this.camera.x * ratio.x,
+            y: this.y + this.camera.y * ratio.y,
+            z: 0
+        }
+
+        renderer.drawRect(
+            cameraPos.x,
+            cameraPos.y,
+            cameraSize,
+            cameraSize,
+            Color.GREEN
+        )
     }
 }
