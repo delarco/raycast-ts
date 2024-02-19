439e55ccc9b943942d3ac952a02e1b925c6bb1b1
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-19 00:05:03 -0300

    implement Minimap game object

--- src/objects/Minimap.ts
+++ src/objects/Minimap.ts
@@ -5,6 +5,9 @@ import { Map } from "../models/Map";
 
 export class Minimap extends GameObject {
 
+    private tileSize: number
+    private tileColor = new Color(100, 100, 100)
+
     constructor(x: number, y: number, width: number, height: number, private map: Map) {
         super()
 
@@ -12,16 +15,27 @@ export class Minimap extends GameObject {
         this.y = y
         this.width = width
         this.height = height
+        this.tileSize = Math.floor(this.width / this.map.size.width)
     }
 
     public override draw(renderer: Renderer): void {
 
-        renderer.drawRect(
-            this.x,
-            this.y,
-            this.width,
-            this.height,
-            Color.GREEN
-        )
+        for (const row of new Array(this.map.size.height).keys()) {
+            for (const col of new Array(this.map.size.width).keys()) {
+
+                const tile = this.map.getTile(col, row)
+
+                if(tile === 1) {
+
+                    renderer.drawRect(
+                        this.x + col *  this.tileSize,
+                        this.y + row *  this.tileSize,
+                        this.tileSize,
+                        this.tileSize,
+                        this.tileColor
+                    )
+                }
+            }
+        }
     }
 }
