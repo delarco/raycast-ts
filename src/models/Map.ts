236252deddbf6a84d1612828bfa3e21c46fe57f3
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-19 07:23:13 -0300

    add isLocationSolid method to Map

--- src/models/Map.ts
+++ src/models/Map.ts
@@ -7,9 +7,17 @@ export class Map {
 
     public getTile(x: number, y: number): number | null {
 
-        if (x >= this.size.width) return null
-        if (y >= this.size.height) return null
+        if (x < 0 || x >= this.size.width) return null
+        if (y < 0 || y >= this.size.height) return null
 
         return this.tiles[x + (y * this.size.width)]
+    }
+
+    public isLocationSolid(x: number, y: number): boolean {
+
+        if (x < 0 || x >= this.size.width) return false
+        if (y < 0 || y >= this.size.height) return false
+
+        return this.tiles[y * this.size.width + x] === 1
     }
 }
