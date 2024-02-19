543b5f4c9a66234761e79972b1f19bcd0e0aef07
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-19 00:04:35 -0300

    refactor RaycastScene

--- src/scenes/RaycastScene.ts
+++ src/scenes/RaycastScene.ts
@@ -1,22 +1,52 @@
 import { Renderer } from "../interfaces/Renderer";
 import { Scene } from "../models/Scene"
+import { GameObject } from "../models/GameObject";
+import { Map } from "../models/Map";
+import { Minimap } from "../objects/Minimap";
+import { MovingRectangle } from "../objects/MovingRectangle";
 import { Clock } from "../utils/Clock";
+import { Color } from "../models/Color";
+import { Size } from "../interfaces/Size";
+import { Position } from "../interfaces/Position";
 
 export class RaycastScene extends Scene {
 
-    preload(): Promise<void> {
-        throw new Error("Method not implemented.");
+    private map: Map
+
+    public async preload(): Promise<void> {
+
+        // load map
+        this.map = new Map()
+        this.map.size = { width: 10, height: 10 }
+        this.map.tiles = [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 1, 1, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 1, 1, 0, 1,
+            1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ]
     }
 
-    init(): void {
-        throw new Error("Method not implemented.");
-    }
+    public init(): void {
 
-    update(clock: Clock): void {
-        throw new Error("Method not implemented.");
-    }
 
-    draw(renderer: Renderer) {
-        throw new Error("Method not implemented.");
+        const minimapSize = Math.floor(this.gameInstance.resolution.width / 4)
+
+        const minimapPos: Position = {
+            x: this.gameInstance.resolution.width - minimapSize - 4,
+            y: 4,
+            z: 0
+        }
+
+        const minimap = new Minimap(minimapPos.x, minimapPos.y, minimapSize, minimapSize, this.map)
+        const box = new MovingRectangle(0, 0, 25, 25, Color.RED)
+
+        this.add(minimap)
+        this.add(box)
     }
 }
