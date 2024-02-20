3590d2db5625909f89299ebb7fe53d2c5574f7e9
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-20 19:04:07 -0300

    refactor Tile class

--- src/models/Tile.ts
+++ src/models/Tile.ts
@@ -1,29 +1,41 @@
 import { Side } from "../enums/Side";
 import { Color } from "./Color";
 import { Vec2D } from "./Vec2D";
+import { Texture } from "./Texture"
 
 export class Tile {
 
     constructor(
         public position: Vec2D,
         public solid: boolean,
-        public texture?: { [key in Side]: Color | null } | null,
-        public detail?: { [key in Side]: Color | null } | null,
+        public texture: { [key in Side]: Texture } | Texture = Texture.fromColor(Color.WHITE),
+        public detail: { [key in Side]: Texture | null } | Texture | null,
     ) {
 
-        if (!texture) {
+        if (texture instanceof Texture) {
 
             this.texture = {
-                [Side.NORTH]: null,
-                [Side.SOUTH]: null,
-                [Side.WEST]: null,
-                [Side.EAST]: null,
-                [Side.TOP]: null,
-                [Side.BOTTOM]: null,
+                [Side.NORTH]: texture,
+                [Side.SOUTH]: texture,
+                [Side.WEST]: texture,
+                [Side.EAST]: texture,
+                [Side.TOP]: texture,
+                [Side.BOTTOM]: texture,
             };
         }
 
-        if (!detail) {
+        if (detail instanceof Texture) {
+
+            this.detail = {
+                [Side.NORTH]: detail,
+                [Side.SOUTH]: detail,
+                [Side.WEST]: detail,
+                [Side.EAST]: detail,
+                [Side.TOP]: detail,
+                [Side.BOTTOM]: detail,
+            };
+        }
+        else if (!detail) {
 
             this.detail = {
                 [Side.NORTH]: null,
