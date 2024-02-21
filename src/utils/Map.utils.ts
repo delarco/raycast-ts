e9f8c78f958d5056cb95eac8c5b5d10bfe5b0803
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-21 06:46:25 -0300

    refactor Map class

--- src/utils/Map.utils.ts
+++ src/utils/Map.utils.ts
@@ -1,5 +1,7 @@
 import { Size } from "../interfaces/Size";
+import { Color } from "../models/Color";
 import { Map } from "../models/Map";
+import { Texture } from "../models/Texture";
 import { Tile } from "../models/Tile";
 import { Vec2D } from "../models/Vec2D";
 
@@ -9,8 +11,12 @@ export class MapUtils {
 
         const map = new Map()
         map.size = size
-        map.tiles = array.map(n => new Tile(new Vec2D(), n === 1))
+        map.tiles = array.map(n => new Tile(
+            new Vec2D(),
+            n === 1,
+            Texture.fromColor(Color.WHITE),
+            null
+        ))
         return map
-
     }
 }
