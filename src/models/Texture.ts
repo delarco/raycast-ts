ec71cf24f300b0fa9357b92e36da9fa1fe30494a
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-21 09:50:10 -0300

    add method to load texture from file

--- src/models/Texture.ts
+++ src/models/Texture.ts
@@ -73,4 +73,66 @@ export class Texture {
         if (debugBorders) texture.drawDebugBorders()
         return texture
     }
+
+    public static fromFile(name: string, filepath: string, debugBorders = false): Promise<Texture> {
+
+        return new Promise((resolve, reject) => {
+
+            fetch(filepath)
+                .then(async result => {
+
+                    const fileExt = filepath.split(/[#?]/)[0]?.split('.')?.pop()?.trim().toUpperCase()
+
+                    let texture: Texture | null = null;
+
+                    switch (fileExt) {
+
+                        case 'JPG':
+                        case 'PNG':
+
+                            const bitmap = await createImageBitmap(await result.blob())
+                            const canvas = new OffscreenCanvas(bitmap.width, bitmap.height)
+                            const context = canvas.getContext('2d')
+
+                            if (!context) throw new Error("Can't get OffscreenCanvas context")
+
+                            context.drawImage(bitmap, 0, 0)
+                            const imageData = context.getImageData(0, 0, bitmap.width, bitmap.height)
+                            const colorBytes = imageData.data.length / (imageData.width * imageData.height)
+
+                            if (colorBytes != 4) {
+                                throw new Error('Not 4-byte color')
+                            }
+
+                            const data: Array<Color> = []
+
+                            for (let index = 0; index < imageData.data.length; index += 4) {
+
+                                data.push(new Color(
+                                    imageData.data[index + 0],
+                                    imageData.data[index + 1],
+                                    imageData.data[index + 2],
+                                    imageData.data[index + 3]
+                                ))
+                            }
+
+                            texture = new Texture(name, imageData.width, imageData.height, data)
+                            break
+
+                        default:
+                            throw new Error(`Unknown file extension: ${fileExt}`)
+                    }
+
+                    if (debugBorders) texture.drawDebugBorders()
+
+                    if (texture) return resolve(texture)
+                    throw new Error("Error loading texture.")
+                })
+                .catch(error => {
+                    console.log(error);
+                    
+                    reject(`Error loading texture: ${name} ${filepath}.`)
+                })
+        })
+    }
 }
