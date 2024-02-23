1087c151c8247068fb867fc229e4a708c32a78da
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-23 01:01:27 -0300

    refactor Rectangle to draw color or texture

--- src/objects/Rectangle.ts
+++ src/objects/Rectangle.ts
@@ -1,10 +1,12 @@
 import { Renderer } from "../interfaces/Renderer";
 import { Color } from "../models/Color";
 import { GameObject } from "../models/GameObject";
+import { Texture } from "../models/Texture";
+import { Vec2D } from "../models/Vec2D";
 
 export class Rectangle extends GameObject {
 
-    constructor(x: number, y: number, width: number, height: number, private color = Color.BLUE) {
+    constructor(x: number, y: number, width: number, height: number, private texture: Texture | Color = Color.WHITE) {
         super()
 
         this.x = x
@@ -15,12 +17,25 @@ export class Rectangle extends GameObject {
 
     public override draw(renderer: Renderer): void {
 
-        renderer.drawRect(
-            this.x,
-            this.y,
-            this.width,
-            this.height,
-            this.color
-        )
+        let pixelColor: (tx: number, ty: number) => Color
+
+        if (this.texture instanceof Color) {
+            pixelColor = () => this.texture as Color
+        }
+        else {
+            pixelColor = (tx: number, ty: number) => (this.texture as Texture).sampleColor(tx, ty)
+        }
+
+        const step = new Vec2D(1.0 / this.width, 1.0 / this.height)
+
+        for (let x = 0; x < this.width; x++) {
+
+            for (let y = 0; y < this.height; y++) {
+
+                const color = pixelColor(x * step.x, y * step.y)
+                if (color.a < 255) continue
+                renderer.drawPixel(this.x + x, this.y + y, color)
+            }
+        }
     }
 }
