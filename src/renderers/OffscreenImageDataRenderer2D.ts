ddf86334ea79be0629466a1156eb080bf67480bc
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-22 23:04:34 -0300

    refactor OffscreenImageDataRenderer2D

--- src/renderers/OffscreenImageDataRenderer2D.ts
+++ src/renderers/OffscreenImageDataRenderer2D.ts
@@ -9,6 +9,7 @@ export class OffscreenImageDataRenderer2D implements Renderer {
     private _resolution: Size
     private imageData: ImageData
     private colorBuffer: Uint8ClampedArray
+    private depthBuffer: Array<number>
 
     public get resolution() { return this._resolution }
 
@@ -29,10 +30,15 @@ export class OffscreenImageDataRenderer2D implements Renderer {
 
         this.imageData = this.context.getImageData(0, 0, this.resolution.width, this.resolution.height)
         this.colorBuffer = this.imageData.data
+        this.depthBuffer = [...new Array(this.resolution.width * this.resolution.height)].map(() => Infinity)
     }
 
     clear(): void {
 
+        for (let i = 0; i < this.depthBuffer.length; i++) {
+
+            this.depthBuffer[i] = Infinity;
+        }
     }
 
     flush(): void {
@@ -57,7 +63,29 @@ export class OffscreenImageDataRenderer2D implements Renderer {
         this.colorBuffer[index + 3] = color.a
     }
 
-    drawLine(x1: number, y1: number, x2: number, y2: number, color: Color): void {
+    drawPixelDepth(x: number, y: number, z: number, color: Color, shade: number = 1.0): void {
+
+        [x, y] = [~~x, ~~y]
+
+        if (x < 0
+            || y < 0
+            || x >= this.resolution.width
+            || y >= this.resolution.height) return
+
+        const depthIndex = y * this.resolution.width + x
+
+        if (z > this.depthBuffer[depthIndex]) return
+        this.depthBuffer[depthIndex] = z
+
+        const index = 4 * (y * this.resolution.width + x)
+
+        this.colorBuffer[index + 0] = color.r * shade
+        this.colorBuffer[index + 1] = color.g * shade
+        this.colorBuffer[index + 2] = color.b * shade
+        this.colorBuffer[index + 3] = color.a
+    }
+
+    drawLine(): void {
         throw new Error("Method not implemented.");
     }
 
@@ -70,7 +98,7 @@ export class OffscreenImageDataRenderer2D implements Renderer {
         }
     }
 
-    drawTexture(x: number, y: number, texture: Texture, scale: number): void {
+    drawTexture(x: number, y: number, texture: Texture): void {
 
         // TODO: implement scale
         for (let tx = 0; tx < texture.width; tx++) {
