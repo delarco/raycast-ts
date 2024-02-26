3016d48a001b4dbb72d209ae9f1e8b7d336c2164
Leandro Del Arco <leandrodelarco@outlook.com>
2024-02-26 00:07:12 -0300

    refactor Sprite class

--- src/objects/Sprite.ts
+++ src/objects/Sprite.ts
@@ -2,11 +2,13 @@ import { Clock } from "../utils/Clock";
 import { Color } from "../models/Color";
 import { GameObject } from "../models/GameObject"
 import { Texture } from "../models/Texture";
+import { SpriteParams } from "../interfaces/SpriteParams";
 
 export class Sprite extends GameObject {
 
     private currentFrameCounter = 0
     private lastTime = 0
+    private frames: number
 
     constructor(
         name: string,
@@ -15,10 +17,7 @@ export class Sprite extends GameObject {
         width: number,
         height: number,
         private texture: Texture,
-        private frames: number,
-        private frameWidth: number,
-        private frameHeight: number,
-        private frameChangeTime: number = 200
+        private params: SpriteParams,
     ) {
         super()
         this.name = name
@@ -27,6 +26,9 @@ export class Sprite extends GameObject {
         this.width = width
         this.height = height
         this.texture = texture
+
+        this.frames = Math.trunc(texture.width / params.frameWidth) * Math.trunc(texture.height / params.frameHeight)
+        if (!params.frameChangeTime) params.frameChangeTime = 200
     }
 
     public nextFrame(): void {
@@ -37,19 +39,20 @@ export class Sprite extends GameObject {
 
     public sampleColor(x: number, y: number): Color {
 
-        const offsetX = this.currentFrameCounter * this.frameWidth
+        const offsetX = this.currentFrameCounter * this.params.frameWidth
 
         if (x < 0) x = 1 - x
         if (y < 0) y = 1 - y
 
-        const sx = Math.min(Math.trunc((x * this.frameWidth)), this.frameWidth - 1);
-        const sy = Math.min(Math.trunc((y * this.frameHeight)), this.frameHeight - 1);
+        // TODO: refactor
+        const sx = Math.min(Math.trunc((x * this.params.frameWidth)), this.params.frameWidth - 1);
+        const sy = Math.min(Math.trunc((y * this.params.frameHeight)), this.params.frameHeight - 1);
         return this.texture.getPixelColor(offsetX + sx, sy);
     }
 
     public override update(clock: Clock): void {
 
-        if (clock.time - this.lastTime > this.frameChangeTime) {
+        if (clock.time - this.lastTime > this.params.frameChangeTime!) {
             this.nextFrame()
             this.lastTime = clock.time
         }
